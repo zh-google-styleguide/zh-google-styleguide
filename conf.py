975023703931986d7df501e80dc597b21cd5bd0e
Yang.Y <yang@yangyubo.com>
2015-08-02 12:52:12 +0800

    使用 Read the Docs 主题

--- conf.py
+++ conf.py
@@ -15,9 +15,18 @@ exclude_patterns = ['_build']
 extensions = ['sphinx.ext.pngmath']
 pygments_style = 'sphinx'
 
+# on_rtd is whether we are on readthedocs.org
+import os
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+# otherwise, readthedocs.org uses their theme by default, so no need to specify it
+
 html_title = u'Google 开源项目风格指南'
-html_theme = 'haiku'
-html_theme_path = ['../../../templates/sphinx', ]
 htmlhelp_basename = 'google-styleguide'
 html_add_permalinks = None
 
